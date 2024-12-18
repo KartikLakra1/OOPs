c9b190cd5dbb4719ec7763a9faa51bbba893a60d
Kartik <129858821+KartikLakra1@users.noreply.github.com>
2024-12-18 11:46:51 +0530

    static member variable

--- Static_Member.cpp
+++ Static_Member.cpp
@@ -14,6 +14,10 @@ public:
         b = 5;
         stat++;
     }
+    ~Alpha()
+    {
+        stat--;
+    }
 
     static int stat;
 };
@@ -23,8 +27,16 @@ int main()
 {
     Alpha a1;
     Alpha a2;
+    Alpha *a3 = new Alpha;
+    cout << "BEFORE \n";
     cout << a1.stat << endl;
     cout << a2.stat << endl;
+    cout << Alpha::stat << endl;
+    cout << "AFTER deleting a3 \n";
+    delete a3;
+    cout << a1.stat << endl;
+    cout << a2.stat << endl;
+    cout << Alpha::stat << endl;
 
     return 0;
 }
